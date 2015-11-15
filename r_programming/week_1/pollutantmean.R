3217a14ad543db1ee69dc6a16b067d593625b500
jdglover <justin.glover01@gmail.com>
2015-11-14 20:51:17 -0500

    removed some commented out code

--- r_programming/week_1/pollutantmean.R
+++ r_programming/week_1/pollutantmean.R
@@ -16,19 +16,7 @@ pollutantmean <- function(directory, pollutant, id=1:332) {
     master_df <- data.frame(matrix(ncol = 4, nrow = 0))
     for (each_csv in data_files[id]) {
         temp_df <- read.csv(paste(wd, each_csv, sep="/"))
-        #print(head(temp_df))
         master_df <- rbind(master_df, temp_df)
-        #print(master_df)
-        #print(colnames(master_df))
     }
-    #master_file <- read.csv(paste(wd, data_files[id[1]], sep="/"))
-    #if(length(id) > 1) {
-    #    idx <- 2
-    #    for (i in id[2:length(id)]) {
-    #        next_file <- read.csv(paste(wd, data_files[id[idx]], sep="/"))
-    #        master_file <- rbind(master_file, next_file)
-    #        idx <- idx + 1
-    #    }
-    #}
     return(mean(master_df[[pollutant]], na.rm=TRUE))
 }
