a5452afac7da45598ec1e47dcce36ac89032e0bc
jdglover <justin.glover01@gmail.com>
2015-11-14 20:50:15 -0500

    refactored for loop

--- r_programming/week_1/pollutantmean.R
+++ r_programming/week_1/pollutantmean.R
@@ -1,4 +1,4 @@
-pollutantmean <- function(directory, pollutant, id) {
+pollutantmean <- function(directory, pollutant, id=1:332) {
     # Computes the mean of the pollutant across all monitors ignoring
     # NA values.
     #
@@ -13,14 +13,22 @@ pollutantmean <- function(directory, pollutant, id) {
     #    (ignoring NA values)
     wd <- paste(getwd(), directory, sep="/")
     data_files <- list.files(wd, pattern="*.csv")
-    master_file <- read.csv(paste(wd, data_files[id[1]], sep="/"))
-    if(length(id) > 1) {
-        idx <- 2
-        for (i in id[2:length(id)]) {
-            next_file <- read.csv(paste(wd, data_files[id[idx]], sep="/"))
-            master_file <- rbind(master_file, next_file)
-            idx <- idx + 1
-        }
+    master_df <- data.frame(matrix(ncol = 4, nrow = 0))
+    for (each_csv in data_files[id]) {
+        temp_df <- read.csv(paste(wd, each_csv, sep="/"))
+        #print(head(temp_df))
+        master_df <- rbind(master_df, temp_df)
+        #print(master_df)
+        #print(colnames(master_df))
     }
-    return(mean(master_file[[pollutant]], na.rm=TRUE))
+    #master_file <- read.csv(paste(wd, data_files[id[1]], sep="/"))
+    #if(length(id) > 1) {
+    #    idx <- 2
+    #    for (i in id[2:length(id)]) {
+    #        next_file <- read.csv(paste(wd, data_files[id[idx]], sep="/"))
+    #        master_file <- rbind(master_file, next_file)
+    #        idx <- idx + 1
+    #    }
+    #}
+    return(mean(master_df[[pollutant]], na.rm=TRUE))
 }
