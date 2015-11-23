1075000735e402beab677aa8f87d5fb7908a85d8
jdglover <justin.glover01@gmail.com>
2015-11-22 22:19:30 -0500

    update function comments and reformat

--- r_programming/week_4/best.R
+++ r_programming/week_4/best.R
@@ -1,4 +1,14 @@
 best <- function(state, outcome) {
+    # Determines the best hospital in a state for a certain condition.
+    #
+    # Args:
+    #    state: A character vector of length 1 that is the two letter
+    #           abbreviation for the state of interest (e.g., "PA").
+    #    outcome: A character vector of length 1. Possible conditions are 
+    #           "heart attack", "heart failure", and "pneumonia".
+    # Returns:
+    #    A character vector indicating the best hospital in a state a 
+    #    particular outcome.
     data <- read.csv("outcome-of-care-measures.csv", stringsAsFactors = FALSE)
     
     poss_states <- unique(data$State)
@@ -23,15 +33,21 @@ best <- function(state, outcome) {
     state_results <- subset.data.frame(relevant_data, hospital_state==state)
 
     if(outcome == "heart attack") {
-        winner <- state_results[which(state_results$heart_attack == min(state_results$heart_attack, na.rm = TRUE)),]
+        winner <- state_results[which(state_results$heart_attack == 
+                                      min(state_results$heart_attack, 
+                                          na.rm = TRUE)),]
         return(winner$hospital)
     }
     if(outcome == "heart failure") {
-        winner <- state_results[which(state_results$heart_failure == min(state_results$heart_failure, na.rm = TRUE)),]
+        winner <- state_results[which(state_results$heart_failure == 
+                                      min(state_results$heart_failure, 
+                                          na.rm = TRUE)),]
         return(winner$hospital)
     }
     if(outcome == "pneumonia") {
-        winner <- state_results[which(state_results$pneumonia == min(state_results$pneumonia, na.rm = TRUE)),]
+        winner <- state_results[which(state_results$pneumonia == 
+                                      min(state_results$pneumonia, 
+                                      na.rm = TRUE)),]
         return(winner$hospital)
     }
 }
